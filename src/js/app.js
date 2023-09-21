5d04a17fc39e6d12bd234c006196594b29e6d69f
3p-vln <vldknovak@gmail.com>
2023-09-21 16:15:40 +0300

    блок с новостями без адаптивов

--- src/js/app.js
+++ src/js/app.js
@@ -11,18 +11,23 @@ menuBtn.addEventListener('click', function () {
 })
 
 let videoBtn = document.querySelector('.play-block');
-videoBtn.addEventListener('click', function(){
+videoBtn.addEventListener('click', function () {
     videoBtn.classList.toggle('active');
 })
 
 let videoBtnSwiper = document.querySelector('.block-2__background');
-videoBtnSwiper.addEventListener('click', function(){
+videoBtnSwiper.addEventListener('click', function () {
     videoBtnSwiper.classList.toggle('active');
 })
 
 let videoBtn_2 = document.querySelector('.play-block-2');
-videoBtn_2.addEventListener('click', function(){
+videoBtn_2.addEventListener('click', function () {
     videoBtn_2.classList.toggle('active');
+})
+
+let videoBtn_new = document.querySelector('.card__video');
+videoBtn_new.addEventListener('click', function () {
+    videoBtn_new.classList.toggle('active');
 })
 
 var swiper = new Swiper(".product-swiper", {
@@ -41,4 +46,23 @@ var swiper = new Swiper(".block-2__swiper", {
         prevEl: '.swiper-button-prev',
     },
     keyboard: true,
+});
+
+var swiper = new Swiper(".news__swiper", {
+    spaceBetween: 0,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    keyboard: true,
+    breakpoints: {
+        1920:{
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1350: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+    }
 });
