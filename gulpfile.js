22403e614e2757f7019462519f13f41b1e2d7e78
HunterAkeriano <stajectgamer@gmail.com>
2023-09-28 17:44:58 +0300

    master: add text

--- gulpfile.js
+++ gulpfile.js
@@ -29,3 +29,5 @@ const mainTasks = gulp.parallel(copy, html, scss, js, images);
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 gulp.task('default', dev);
+
+// привет
