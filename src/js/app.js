802282b058f7b555cb614fd4f29847cc31284cbd
3p-vln <vldknovak@gmail.com>
2023-10-09 20:12:46 +0300

    десктоп верстка блока с табами

--- src/js/app.js
+++ src/js/app.js
@@ -17,17 +17,6 @@ video.forEach(b=>b.addEventListener('click', function () {
     );
 }));
 
-
-// function initPlayVideo() {
-//     $(".video-cover").on("click", function () {
-//         $(this).fadeOut().siblings('.video').html(
-//             '<iframe src="https://www.youtube.com/embed/hT4O8VI__XE?si=oyVRl9Byye2mLfrt' + $(".f-video-cover").data("video") + '?feature=oembed&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-//         );
-//     });
-// };
-
-// initPlayVideo();
-
 var swiper = new Swiper(".product-swiper", {
     spaceBetween: 30,
     navigation: {
@@ -86,33 +75,3 @@ let zoom = document.querySelector('.short-about-nut');
 zoomBtn.addEventListener('click', function () {
     zoom.classList.toggle('active');
 });
-
-const tabsBtn   = document.querySelectorAll(".tabs__nav-btn");
-const tabsItems = document.querySelectorAll(".tabs__item");
-
-tabsBtn.forEach(onTabClick);
-
-function onTabClick(item) {
-    item.addEventListener("click", function() {
-        let currentBtn = item;
-        let tabId = currentBtn.getAttribute("data-tab");
-        let currentTab = document.querySelector(tabId);
-
-        if( ! currentBtn.classList.contains('active') ) {
-            tabsBtn.forEach(function(item) {
-                item.classList.remove('active');
-            });
-    
-            tabsItems.forEach(function(item) {
-                item.classList.remove('active');
-            });
-    
-            currentBtn.classList.add('active');
-            currentTab.classList.add('active');
-        }
-    });
-}
-
-document.querySelector('.tabs__nav-btn').click();
-
-
