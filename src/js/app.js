359aa838568dabf00c183126cdde9702a2187350
3p-vln <vldknovak@gmail.com>
2023-10-16 16:23:40 +0300

    испарвление гридов и переключение радиокнопок

--- src/js/app.js
+++ src/js/app.js
@@ -10,92 +10,80 @@ menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active');
 });
 
-var video = document.querySelectorAll('.video-cover');
+if (document.querySelector('.video-cover')) {
+    var video = document.querySelectorAll('.video-cover');
 
-video.forEach(b => b.addEventListener('click', function () {
-    b.insertAdjacentHTML("afterbegin", '<iframe src="https://www.youtube.com/embed/hT4O8VI__XE?si=oyVRl9Byye2mLfrt?feature=oembed&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-    );
-}));
+    video.forEach(b => b.addEventListener('click', function () {
+        b.insertAdjacentHTML("afterbegin", '<iframe src="https://www.youtube.com/embed/hT4O8VI__XE?si=oyVRl9Byye2mLfrt?feature=oembed&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        );
+    }));
 
-var swiper = new Swiper(".product-swiper", {
-    spaceBetween: 30,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    keyboard: true,
-});
+}
 
-var swiper = new Swiper(".block-two__swiper", {
-    spaceBetween: 0,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    keyboard: true,
-});
-
-var swiper = new Swiper(".news__swiper", {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    keyboard: true,
-    breakpoints: {
-        375: {
-            slidesPerView: 1,
-            spaceBetween: 30,
+if (document.querySelector('.product-swiper')) {
+    new Swiper(".product-swiper", {
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        920: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+        keyboard: true,
+    });
+}
+
+if (document.querySelector('.block-two__swiper')) {
+    new Swiper(".block-two__swiper", {
+        spaceBetween: 0,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        1360: {
-            slidesPerView: 3,
-            spaceBetween: 30,
+        keyboard: true,
+    });
+}
+
+if (document.querySelector('.news__swiper')) {
+    new Swiper(".news__swiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: true,
+        breakpoints: {
+            375: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+            },
+            920: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1360: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            }
         }
-    }
-});
+    });
+}
 
+if (document.querySelector('.short-about-nut__swiper')) {
+    new Swiper(".short-about-nut__swiper", {
+        spaceBetween: 0,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: true,
+    });
+}
 
-var swiper = new Swiper(".short-about-nut__swiper", {
-    spaceBetween: 0,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    keyboard: true,
-});
-
-
-let zoomBtn = document.querySelector('.short-about-nut .zoom');
 let zoom = document.querySelector('.short-about-nut');
-zoomBtn.addEventListener('click', function () {
-    zoom.classList.toggle('active');
-});
+let zoomBtn = document.querySelector('.short-about-nut .zoom');
+if (zoom && zoomBtn) {
+    zoomBtn.addEventListener('click', function () {
+        zoom.classList.toggle('active');
+    });
+}
 
-
-const radioBtn = document.querySelectorAll(".registration__radio-btn");
-const radioItems = document.querySelectorAll(".radio__item");
-radioBtn.forEach(item => {
-    item.addEventListener("click", function (){
-        const currentRadioBtn = item;
-        const radioId = currentBtn.getAttribute("data-radio");
-        const currentRadio = document.querySelector(radioId);
-
-        if (!currentRadioBtn.classList.contains('active')) {
-            radioBtn.forEach(function (item) {
-                item.classList.remove('active');
-            });
-
-            radioItems.forEach(function (item) {
-                item.classList.remove('active');
-            });
-
-            currentRadioBtn.classList.add('active');
-            currentRadio.classList.add('active');
-        }
-    }
-)});
